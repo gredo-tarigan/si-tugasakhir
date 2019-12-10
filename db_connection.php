3765546e8a43e9e603000f1ab8e686489755d9d1
gredo_tarigant <jegredotigan@gmail.com>
2019-12-11 00:36:18 +0700

    Update tujuan database nya ke remotemysql

--- db_connection.php
+++ db_connection.php
@@ -1,10 +1,10 @@
 <?php
 function OpenCon()
  {
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = '';
- $db = "teknik_industri";
+ $dbhost = "remotemysql.com";
+ $dbuser = "4zfOiEEhb1";
+ $dbpass = "j7mrJstwWY";
+ $db = "4zfOiEEhb1";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
  return $conn;
